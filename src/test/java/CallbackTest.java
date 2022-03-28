6f3dfada91736a45176848adad239a22ef6ea75c
Ilnur Mameshev <imameshev@inbox.ru>
2022-03-29 00:57:01 +0300

    added test

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -2,9 +2,13 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CallbackTest {
     private WebDriver driver;
@@ -27,5 +31,19 @@ public class CallbackTest {
     public void tearDown() {
         driver.quit();
         driver = null;
+    }
+
+    @Test
+    public void shouldSuccessFullCard() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Василий Пупкин");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79123457888");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+        String expected ="Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(expected,text);
+
+
     }
 }
