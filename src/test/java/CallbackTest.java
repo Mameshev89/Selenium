62a38f83716dcb1a24b45acb72e523ea6b76ccfe
Ilnur Mameshev <imameshev@inbox.ru>
2022-03-28 20:51:28 +0300

    added adjusts

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -1,25 +1,31 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class CallbackTest {
     private WebDriver driver;
 
     @BeforeAll
-    public static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+    public static void setUpClass() {
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
     public void setUp() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
     public void tearDown() {
         driver.quit();
-        driver=null;
+        driver = null;
     }
 }
